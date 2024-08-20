10e7051879346da4ec9946f765aff2301839a10e
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-08-20 10:42:52 +0300

    C is fun

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -19,10 +19,10 @@ def hbnb():
     return "HBNB"
 
 
-@app.route('/C/<text>', strict_slashes=False)
+@app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """Displays 'C' then the value of the text variable"""
-    return "C " + text.replace('_', ' ')
+    return "C" + text.replace('_', ' ')
 
 
 if __name__ == "__main__":
